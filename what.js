c5565df888310a8848dd2efa4634f8a0d9bb1ca7
joechesney <33613119+joechesney@users.noreply.github.com>
2017-11-29 09:00:44 -0600

    finished all the challenges of the exercise

--- what.js
+++ what.js
@@ -45,7 +45,7 @@ let containerDiv = document.getElementById("container");
 
 for (let i = 0; i < celebs.length; i++) {
     let newContent = "<header>" + celebs[i].name + ", " + celebs[i].title + "</header>";
-    newContent += `<bio>${celebs[i].bio}</bio><br><img src="${celebs[i].image}"/>`;
+    newContent += `<bio id="bio${i}">${celebs[i].bio}</bio><br><img src="${celebs[i].image}"/>`;
     newContent += `<br><lifespan>Born: ${celebs[i].lifespan.birth}<br>`;
     newContent += `Died: ${celebs[i].lifespan.death}</lifespan>`;
 
@@ -59,18 +59,39 @@ for (let i = 0; i < celebs.length; i++) {
 
 // declare textBox and assign en event listener to every element in the container
 const textBox = document.getElementById("inputBox");
-const containerDivElements = document.getElementsByTagName("*");
+const containerDivElements = document.getElementsByClassName("oneCard");
 for (var j = 0; j < containerDivElements.length; j++) {
     containerDivElements[j].addEventListener("click", function (event) {
+        textBox.value = "";
         for (var y = 0; y < containerDivElements.length; y++){
-            containerDivElements[y].setAttribute("style", "border: 0;");
+            containerDivElements[y].setAttribute("style", "border: none;");
             console.log("border removed");
         }
-        containerDivElements[j].setAttribute("style", " border: 3px dotted lightblue;");
+        this.setAttribute("style", " border: 3px dotted black;"); // 'this' fixed everything!!!
         textBox.focus();
         console.log("before focus");
     });
+    
 };
+
+
+// const bioInEl = document.getElementById();
 // // textbox sync with bio
 // textBox.addEventListener("click")
 // textBox.value;
+
+textBox.addEventListener("keyup", syncBio);
+ 
+function syncBio() {
+    for (let d = 0; d < containerDivElements.length; d++){
+        if (document.getElementById(`bio${d}`).parentElement.style.border != "none"){
+            var bioBox = document.getElementById(`bio${d}`);
+        }
+    }
+    if(event.keyCode != 13) {
+        bioBox.innerHTML = textBox.value;
+    }else if(event.keyCode === 13){
+        textBox.value = "";
+    }
+}
+
